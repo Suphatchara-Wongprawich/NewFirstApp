fd5defcc01d0906b9e0aea5e14241eca961881a4
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-25 20:29:05 +0700

    NewFirstApp:Add SleepDatabase

---
+++ app/src/main/java/com/example/firstapp/SleepDatabase.kt
@@ -0,0 +1,41 @@
+package com.example.firstapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Creating Database
+ * Database Require Entities Argument to show Construction that Database is for?
+ * */
+
+@Database(entities = [SleepDatabase::class],version = 1,exportSchema = false)
+abstract class SleepDatabase: RoomDatabase(){
+    abstract val sleepDatabaseDao:SleepDatabaseDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context):SleepDatabase{
+
+            synchronized(this){
+
+                var instance = INSTANCE
+
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database").fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+    }
+}
