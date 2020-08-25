83d124d72b6e1b5565e4aafd5e531f396c8040ee
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-26 03:58:19 +0700

    NewFirstApp:modify All

---
+++ app/src/main/java/com/example/firstapp/Newactivity.kt
@@ -0,0 +1,37 @@
+package com.example.firstapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class Newactivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_activity)
+        editText = findViewById(R.id.editText)
+
+        val button = findViewById<Button>(R.id.button_add)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editText.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val user = editText.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, user)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
